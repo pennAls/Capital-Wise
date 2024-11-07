aea6035d4c17c0f72e0e45c18fdb7b2c94b958db
Lucas Pena Araújo dos Santos <143518683+pennAls@users.noreply.github.com>
2024-11-07 17:59:40 -0300

    Ajustes de Navegação

--- Capital_Wise/grafico/grafico.js
+++ Capital_Wise/grafico/grafico.js
@@ -2,7 +2,7 @@ window.onload = function () {
     const canvas = document.getElementById('lineChart');
     const ctx = canvas.getContext('2d');
 
-    // Define a largura e altura do canvas com base nas dimensões da caixa
+   
     canvas.width = canvas.parentElement.clientWidth;
     canvas.height = canvas.parentElement.clientHeight - 30; // Reduz altura para acomodar o título
 
@@ -22,3 +22,47 @@ window.onload = function () {
     
     ctx.stroke();
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    const userButton = document.querySelector(".wallet-buttom");
+  
+    const loggedInEmail = sessionStorage.getItem("loggedInUserEmail");
+    const loggedInUser = users.find((user) => user.email === loggedInEmail);
+  
+    if (loggedInUser) {
+      userButton.innerText = loggedInUser.name;
+    } else {
+      userButton.innerText = "Usuário Desconhecido";
+    }
+  
+    class Navigation {
+      navigateToMain() {
+        window.location.href = "/Capital_Wise/Tela_main/main.html";
+      }
+      navigateToUserScreen() {
+        window.location.href = "/Capital_Wise/Tela_Usuario/usuario.html";
+      }
+    }
+  
+    const navigate = new Navigation();
+  
+    const sairButton = document.getElementsByClassName("sair-buttom")[0];
+  
+    sairButton.addEventListener("click", () => navigate.navigateToMain());
+    userButton.addEventListener("click", () => navigate.navigateToUserScreen());
+  
+    const timeOutSession = () => {
+      sessionStorage.clear();
+      window.location.href = "/Capital_Wise/Tela_main/main.html";
+    };
+  
+    let tempoParaTimeout = 100000;
+  
+    const haveToken = sessionStorage.getItem("token") || false;
+  
+    if (haveToken) {
+      setTimeout(timeOutSession, tempoParaTimeout);
+    }
+  });
+  
--- Capital_Wise/investimento_usuario/investimentos.js
+++ Capital_Wise/investimento_usuario/investimentos.js
@@ -1,43 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const users = JSON.parse(localStorage.getItem("users")) || [];
-    const userButton = document.querySelector(".wallet-buttom");
-  
-    const loggedInEmail = sessionStorage.getItem("loggedInUserEmail");
-    const loggedInUser = users.find((user) => user.email === loggedInEmail);
-  
-    if (loggedInUser) {
-      userButton.innerText = loggedInUser.name;
-    } else {
-      userButton.innerText = "Usuário Desconhecido";
-    }
-  
-    class Navigation {
-      navigateToMain() {
-        window.location.href = "/Capital_Wise/Tela_main/main.html";
-      }
-      navigateToUserScreen() {
-          window.location.href = "/Capital_Wise/Tela_Usuario/usuario.html";
-        }
-    }
-  
-    const navigate = new Navigation();
-  
-    
-    const sairButton = document.getElementsByClassName("sair-buttom")[0];
-  
-    sairButton.addEventListener("click", () => navigate.navigateToMain());
-    userButton.addEventListener("click", () => navigate.navigateToUserScreen());
-  
-    const timeOutSession = () => {
-      sessionStorage.clear();
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+  const userButton = document.querySelector(".wallet-buttom");
+
+  const loggedInEmail = sessionStorage.getItem("loggedInUserEmail");
+  const loggedInUser = users.find((user) => user.email === loggedInEmail);
+
+  if (loggedInUser) {
+    userButton.innerText = loggedInUser.name;
+  } else {
+    userButton.innerText = "Usuário Desconhecido";
+  }
+
+  class Navigation {
+    navigateToMain() {
       window.location.href = "/Capital_Wise/Tela_main/main.html";
-    };
-  
-    let tempoParaTimeout = 100000;
-  
-    const haveToken = sessionStorage.getItem("token") || false;
-  
-    if (haveToken) {
-      setTimeout(timeOutSession, tempoParaTimeout);
     }
-  });
+    navigateToUserScreen() {
+      window.location.href = "/Capital_Wise/Tela_Usuario/usuario.html";
+    }
+  }
+
+  const navigate = new Navigation();
+
+  const sairButton = document.getElementsByClassName("sair-buttom")[0];
+
+  sairButton.addEventListener("click", () => navigate.navigateToMain());
+  userButton.addEventListener("click", () => navigate.navigateToUserScreen());
+
+  const timeOutSession = () => {
+    sessionStorage.clear();
+    window.location.href = "/Capital_Wise/Tela_main/main.html";
+  };
+
+  let tempoParaTimeout = 100000;
+
+  const haveToken = sessionStorage.getItem("token") || false;
+
+  if (haveToken) {
+    setTimeout(timeOutSession, tempoParaTimeout);
+  }
+});
